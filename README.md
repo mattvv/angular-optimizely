21d588dd6952f1fa1fc110a1d73edc10f5b62170
Matt Van Veenendaal <m@mattvv.com>
2014-01-31 14:21:07 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# angular-optmizely
+A simple directive to allow A/B testing with optimizely in angular applications.
+
+## Usage
+1. `bower install angular-optizely`
+2. Include the `ngshowvariant.js` script provided by this component into your app.
+3. Add `ngshowvariant` as a module dependency to your app.
+5. Insert the `ng-show-variant` directive into your template where you wish to conditionally show items.
+
+```html
+<code>
+    <div ng-show-variant="alphabet">variant alphabet is running</div>
+    <div ng-show-variant="cactus">variant cactus is running</div>
+    <div ng-show-variant="cactus,alphabet">variant cactus or alphabet is running</div>
+    <div ng-show-variant="none">No variant is enabled</div>
+    <div ng-show-variant="cactus,none">either variant cactus or no variant is enabled</div>
+ </code>
+```
+
+In optimizely, add custom javascript where you wish to change the variant:
+
+```javascript
+//set which variant you would like
+window.variant= "optmizely1";
+
+//tell angular the variant has changed.
+var scope = angular.element(document.getElementById('main')).injector().get('$rootScope');
+
+scope.$apply( function() {
+  scope.$broadcast('$updateVariant');
+});
+```
+
+## License
+MIT
---
+++ ngshowvariant.js
@@ -0,0 +1,66 @@
+'use strict';
+angular.module('ngshowvariant')
+
+/*
+ * angular-markdown-directive v0.1.0
+ * (c) 2014 Matt Van Veenendaal
+ * License: MIT
+ */
+
+/**
+ * A directive that shows elements only when the given variation state is in effect
+ * Requires a window variable to be set using optimizely.
+ * <code>
+ *    <div ng-show-variant="alphabet">variant alphabet is running</div>
+ *    <div ng-show-variant="cactus">variant cactus is running</div>
+ *    <div ng-show-variant="cactus,alphabet">variant cactus or alphabet is running</div>
+ *    <div ng-show-variant="none">No variant is enabled</div>
+ *    <div ng-show-variant="cactus,none">either variant cactus or no variant is enabled</div>
+ * </code>
+ */
+
+ /**
+ * In optimizely, add this to 'custom javascript'
+ *
+ *
+ * //set which variant you would like
+ * window.variant= "optmizely1";
+ * 
+ * //tell angular the variant has changed.
+ * var scope = angular.element(document.getElementById('main')).injector().get('$rootScope');
+ * 
+ * scope.$apply( function() {
+ *   scope.$broadcast('$updateVariant');
+ * });
+ */
+  .directive('ngShowVariant', function($rootScope) {
+    var variant = window.variant;
+    if (!variant) {
+      variant = 'none';
+    }
+    function inList(needle, list) {
+      var res = false;
+      angular.forEach(list, function(x) {
+        if( x === needle ) {
+          res = true;
+          return true;
+        }
+        return false;
+      });
+      return res;
+    }
+    return {
+      restrict: 'A',
+      compile: function(el, attr) {
+        var expectingVariant = (attr.ngShowVariant||'').split(',');
+        function fn(newVariant) {
+          variant = newVariant;
+          var hide = !inList(newVariant, expectingVariant);
+          el.toggleClass('hide', hide);
+        }
+        fn(variant);
+
+        $rootScope.$on('$updateVariant', function() { fn(window.variant); });
+      }
+    };
+  });
